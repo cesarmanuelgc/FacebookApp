5284a5a2213a8aa8b25e2de21cfd2ef09ad71e3c
cesarmanuelgc@gmail.com <cesarmanuelgc@gmail.com>
2015-10-13 09:47:04 -0500

    Se sube codigo fuente

---
+++ FacebookApp/Models/FacebookMusicViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacebookApp.Models
+{
+    public class FacebookMusicViewModel
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ FacebookApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FacebookApp.Startup))]
+namespace FacebookApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
